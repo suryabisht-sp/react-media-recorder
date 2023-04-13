d60e60141bfb5192cd1ad5e497d63fdac7909cd8
webdev.sp <webdev.sp@amtexsystems.com>
2023-04-13 14:42:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import VideoRecorder from "./components/VideoRecorder";
+import AudioRecorder from "./components/AudioRecorder";
 
-function App() {
+const App = () => {
+  let [recordOption, setRecordOption] = useState("");
+  const toggleRecordOption = (type) => {
+    setRecordOption(type);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Media Recorder</h1>
+      <div className="button-flex">
+        <button onClick={() => toggleRecordOption("video")}>
+          Record Video
+        </button>
+        <button onClick={() => toggleRecordOption("audio")}>
+          Record Audio
+        </button>
+      </div>
+      {recordOption === "audio" && (
+        <div>
+          <AudioRecorder />
+        </div>
+      )}
+
+      {recordOption === "video" && (
+        <div>
+          {" "}
+          <VideoRecorder />
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default App;
